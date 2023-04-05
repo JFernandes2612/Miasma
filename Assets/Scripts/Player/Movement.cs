1a782f6bd8d48378ca7c1f69f814aa59132f728d
Joel Fernandes <up201904977@fe.up.pt>
2023-04-05 22:50:45 +0100

    Improve movement and jumping

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -16,6 +16,8 @@ public class Movement : MonoBehaviour
 
     private PlayerMovement playerMovement;
 
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,10 +32,27 @@ public class Movement : MonoBehaviour
         moveInput = playerMovement.Player_Map.Movement.ReadValue<Vector3>();
         Vector3 inputSpeed = moveInput.normalized * movementSpeed;
         rb.velocity = inputSpeed.x * transform.right + inputSpeed.z * transform.forward + rb.velocity.y * transform.up;
+    }
 
-        // Check jump
-        playerMovement.Player_Map.Jump.performed += ctx => {
+    void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            isGrounded = false;
             rb.AddForce(Vector3.up * jumpForce);
-        };
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.contacts.Length > 0)
+        {
+            foreach (ContactPoint contact in collision.contacts)
+                if (contact.normal.y > 0.0f)
+                {
+                    isGrounded = true;
+                    return;
+                }
+        }
     }
 }
