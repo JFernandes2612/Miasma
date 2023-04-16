7cf362077b5901728df19baccd310dadf856d65d
Joel Fernandes <up201904977@fe.up.pt>
2023-04-16 21:30:59 +0100

    feat: basic navmesh test with menacing capsule monkaW

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(playerTransform.position);
+    }
+}
