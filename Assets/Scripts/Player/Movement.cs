3ee2aeaeae6fcc3db0b6f18e049e5c3389052e72
Miguel Freitas <radiodomiguel70@gmail.com>
2023-04-20 18:33:51 +0100

    Fix jump buffering

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -21,22 +21,27 @@ public class Movement : MonoBehaviour
 
     [SerializeField]
     private float coyoteTime;
-
-    private float jumpBufferCounter;
+    private float coyoteTimeCounter;
 
     [SerializeField]
     private float jumpBufferTime = 0.2f;
-    private float coyoteTimeCounter;
+
+    private float jumpBufferCounter;
+
+
     private Vector3 moveInput;
 
     private Rigidbody rb;
+
+    [SerializeField]
+    private Vector3 boxsize = new Vector3(0.5f, 0.5f, 0.5f);
 
     private PlayerInput playerMovement;
 
     private float distanceToGround;
 
-    private bool canDash = true;
-    private bool isDashing = false;
+    private bool canQuickStep = true;
+    private bool isAnimLocked = false;
 
     [SerializeField]
     private float dashingPower = 24f;
@@ -59,91 +64,106 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Check player movement
-        if (isDashing) return;
-        moveInput = playerMovement.Player_Map.Movement.ReadValue<Vector3>();
+
+        if (isAnimLocked) return;
 
         if (isGrounded())
         {
-
             coyoteTimeCounter = coyoteTime;
-            if (moveInput != Vector3.zero)
+
+            if (canQuickStep && playerMovement.Player_Map.QuickStep.IsPressed())
             {
-                Vector3 inputSpeed = moveInput.normalized * baseMovementSpeed * acceleration;
-                rb.velocity += (inputSpeed.x * transform.right + inputSpeed.z * transform.forward);
-            }
-            else if (rb.velocity != Vector3.zero)
-            {
-                rb.velocity -= rb.velocity * acceleration;
+                StartCoroutine(QuickStep());
             }
 
-            rb.velocity = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z), baseMovementSpeed) + rb.velocity.y * Vector3.up;
-
+            GroundMove();
         }
         else
         {
             coyoteTimeCounter -= Time.deltaTime;
+
+            if (moveInput != Vector3.zero)
+            {
+                AirAccel();
+            }
         }
 
-
+        Debug.Log("coyoteTimeCounter: " + coyoteTimeCounter);
+        Debug.Log("jumpBufferCounter: " + jumpBufferCounter);
+        Debug.Log("isGrounded: " + isGrounded());
         if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
         {
             coyoteTimeCounter = 0f;
+            //reset y velocity
+            rb.velocity = new Vector3(rb.velocity.x, 0.0f, rb.velocity.z);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             jumpBufferCounter = 0f;
-        }
-        if (moveInput != Vector3.zero)
-        {
-
-            Vector3 wishDir = (moveInput.x * transform.right + moveInput.z * transform.forward).normalized;
-            float currentSpeed = Vector3.Dot(rb.velocity, wishDir);
-
-            float addSpeed = baseMovementSpeed - currentSpeed;
-
-            if (addSpeed <= 0.0f)
-                return;
-
-            rb.velocity += addSpeed * airAcceleration * wishDir;
-
-            rb.velocity = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z), maxAirSpeed) + rb.velocity.y * Vector3.up;
-        }
-
-        if (playerMovement.Player_Map.QuickStep.IsPressed() && canDash)
-        {
-            StartCoroutine(Dash());
         }
 
     }
 
     void Update()
     {
+        moveInput = playerMovement.Player_Map.Movement.ReadValue<Vector3>();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
             jumpBufferCounter = jumpBufferTime;
         }
-        else
+        else if (jumpBufferCounter > 0f)
         {
             jumpBufferCounter -= Time.deltaTime;
         }
     }
 
-    private IEnumerator Dash()
+    private IEnumerator QuickStep()
     {
-        isDashing = true;
-        canDash = false;
-        //rb.AddForce(moveInput * dashingPower, ForceMode.Impulse);
-        rb.AddForce(transform.forward * dashingPower, ForceMode.Impulse);
+        isAnimLocked = true;
+        canQuickStep = false;
+        Vector3 wishSpeed = (moveInput.x * transform.right + moveInput.z * transform.forward).normalized;
+        rb.AddForce(wishSpeed * dashingPower, ForceMode.VelocityChange);
         yield return new WaitForSeconds(dashTime);
-        isDashing = false;
+        isAnimLocked = false;
         yield return new WaitForSeconds(dashCooldown);
-        canDash = true;
+        canQuickStep = true;
     }
 
 
     bool isGrounded()
     {
+        //return Physics.BoxCast(transform.position, boxsize, -transform.up, transform.rotation, distanceToGround + 0.1f);
         return Physics.Raycast(transform.position, Vector3.down, distanceToGround + 0.1f);
+    }
+
+    private void AirAccel()
+    {
+        Vector3 wishDir = (moveInput.x * transform.right + moveInput.z * transform.forward).normalized;
+        float currentSpeed = Vector3.Dot(rb.velocity, wishDir);
+
+        float addSpeed = baseMovementSpeed - currentSpeed;
+
+        if (addSpeed <= 0.0f)
+            return;
+
+        rb.velocity += addSpeed * airAcceleration * wishDir;
+
+        rb.velocity = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z), maxAirSpeed) + rb.velocity.y * Vector3.up;
+    }
+
+    private void GroundMove()
+    {
+        if (moveInput != Vector3.zero)
+        {
+            //move
+            Vector3 inputSpeed = moveInput.normalized * baseMovementSpeed * acceleration;
+            rb.velocity += (inputSpeed.x * transform.right + inputSpeed.z * transform.forward);
+        }
+        else if (rb.velocity != Vector3.zero)
+        {
+            //deccel
+            rb.velocity -= rb.velocity * acceleration;
+        }
+
+        rb.velocity = Vector3.ClampMagnitude(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z), baseMovementSpeed) + rb.velocity.y * Vector3.up;
     }
 }
