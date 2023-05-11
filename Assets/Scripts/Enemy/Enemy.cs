ce1f0fb6e1e917692ae40a97486feb31c9631a57
Jorge Costa <jotac431@gmail.com>
2023-05-11 18:03:00 +0100

    Testing Enemy Walk and Attack Animation states

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,26 +7,57 @@ public class Enemy : Entity
 {
     private Transform playerTransform;
     private NavMeshAgent agent;
+    Animator animator;
+    int attackRange = 2;
+
 
     private float playerDetectionRange = 10.0f;
 
     private bool targetingPlayer = false;
+    private bool isAttacking = false;
+    private float attackStart = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (targetingPlayer || Vector3.Distance(playerTransform.position, transform.position) <= playerDetectionRange)
+        if (Vector3.Distance(playerTransform.position, transform.position) <= attackRange)
+        {
+            Attack();
+            attackStart = Time.time;
+        }
+
+        if(Time.time - attackStart >= 3)
+        {
+            animator.SetBool("isAttacking", false);
+            isAttacking = false;
+        } 
+
+        if (!isAttacking && (targetingPlayer || Vector3.Distance(playerTransform.position, transform.position) <= playerDetectionRange))
         {
             targetingPlayer = true;
             agent.SetDestination(playerTransform.position);
+            Walk();
         }
+    }
+
+    void Walk()
+    {
+        animator.SetBool("isWalking", true);
+    }
+
+    void Attack()
+    {
+        animator.SetBool("isWalking", false);
+        isAttacking = true;
+        animator.SetBool("isAttacking", true);
     }
 
     override protected void Death()
