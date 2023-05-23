05223a1f9e0d13d7b83f67e493e449928b06252e
Jorge Costa <jotac431@gmail.com>
2023-05-23 18:24:54 +0100

    Idle, Patrolling, Chase, Attack Behaviours

---
+++ Assets/Scripts/Enemy/Behaviours/AttackBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviour : StateMachineBehaviour
+{
+    Transform player;
+    float attackRange;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        attackRange = animator.GetComponent<Enemy>().attackRange;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 lookAtPosition = new Vector3(player.position.x, player.position.y - 1f, player.position.z);
+        animator.transform.LookAt(lookAtPosition);
+        float distance = Vector3.Distance(animator.transform.position, player.position);
+        if (distance > attackRange*2)
+            animator.SetBool("isAttacking", false);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,56 +8,57 @@ public class Enemy : Entity
     private Transform playerTransform;
     private NavMeshAgent agent;
     Animator animator;
-    int attackRange = 2;
+    public int attackRange = 3;
 
 
-    private float playerDetectionRange = 10.0f;
+    /*private float playerDetectionRange = 10.0f;
 
     private bool targetingPlayer = false;
-    private bool isAttacking = false;
-    private float attackStart = 0;
+   private bool isAttacking = false;
+    private float attackStart = 0;*/
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        /*agent = GetComponent<NavMeshAgent>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(playerTransform.position, transform.position) <= attackRange)
+        /*if (Vector3.Distance(playerTransform.position, transform.position) <= attackRange)
         {
             Attack();
-            attackStart = Time.time;
-        }
-
-        if(Time.time - attackStart >= 3)
-        {
-            animator.SetBool("isAttacking", false);
-            isAttacking = false;
         } 
 
+        if (Time.time - attackStart >= 3)
+        {
+            //animator.SetBool("isAttacking", false);
+            //isAttacking = false;
+        }
+        
+        
         if (!isAttacking && (targetingPlayer || Vector3.Distance(playerTransform.position, transform.position) <= playerDetectionRange))
         {
             targetingPlayer = true;
             agent.SetDestination(playerTransform.position);
             Walk();
-        }
+        }*/
     }
 
     void Walk()
     {
-        animator.SetBool("isWalking", true);
+        //animator.SetBool("isWalking", true);
     }
 
     void Attack()
     {
-        animator.SetBool("isWalking", false);
+        /*animator.SetBool("isWalking", false);
         isAttacking = true;
         animator.SetBool("isAttacking", true);
+        attackStart = Time.time;*/
     }
 
     override protected void Death()
