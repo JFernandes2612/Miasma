e06cceaf6fbb3cb97887d41393a43ab1e8abb94a
Mário Travassos <retchut@protonmail.ch>
2023-06-18 17:31:45 +0100

    feat: implement lunging and broadsword animation switching

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -53,6 +53,15 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""SpecialAttack"",
+                    ""type"": ""Button"",
+                    ""id"": ""f35805d0-634d-480e-b31d-28b4fe089f2a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -132,6 +141,17 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                     ""action"": ""QuickStep"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""189a25e9-c470-4927-8e38-0e91a71a6178"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SpecialAttack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -143,6 +163,7 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         m_Player_Map_Movement = m_Player_Map.FindAction("Movement", throwIfNotFound: true);
         m_Player_Map_Attack = m_Player_Map.FindAction("Attack", throwIfNotFound: true);
         m_Player_Map_QuickStep = m_Player_Map.FindAction("QuickStep", throwIfNotFound: true);
+        m_Player_Map_SpecialAttack = m_Player_Map.FindAction("SpecialAttack", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -205,6 +226,7 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Map_Movement;
     private readonly InputAction m_Player_Map_Attack;
     private readonly InputAction m_Player_Map_QuickStep;
+    private readonly InputAction m_Player_Map_SpecialAttack;
     public struct Player_MapActions
     {
         private @PlayerInput m_Wrapper;
@@ -212,6 +234,7 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         public InputAction @Movement => m_Wrapper.m_Player_Map_Movement;
         public InputAction @Attack => m_Wrapper.m_Player_Map_Attack;
         public InputAction @QuickStep => m_Wrapper.m_Player_Map_QuickStep;
+        public InputAction @SpecialAttack => m_Wrapper.m_Player_Map_SpecialAttack;
         public InputActionMap Get() { return m_Wrapper.m_Player_Map; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -230,6 +253,9 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                 @QuickStep.started -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnQuickStep;
                 @QuickStep.performed -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnQuickStep;
                 @QuickStep.canceled -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnQuickStep;
+                @SpecialAttack.started -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnSpecialAttack;
+                @SpecialAttack.performed -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnSpecialAttack;
+                @SpecialAttack.canceled -= m_Wrapper.m_Player_MapActionsCallbackInterface.OnSpecialAttack;
             }
             m_Wrapper.m_Player_MapActionsCallbackInterface = instance;
             if (instance != null)
@@ -243,6 +269,9 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
                 @QuickStep.started += instance.OnQuickStep;
                 @QuickStep.performed += instance.OnQuickStep;
                 @QuickStep.canceled += instance.OnQuickStep;
+                @SpecialAttack.started += instance.OnSpecialAttack;
+                @SpecialAttack.performed += instance.OnSpecialAttack;
+                @SpecialAttack.canceled += instance.OnSpecialAttack;
             }
         }
     }
@@ -252,5 +281,6 @@ public partial class @PlayerInput : IInputActionCollection2, IDisposable
         void OnMovement(InputAction.CallbackContext context);
         void OnAttack(InputAction.CallbackContext context);
         void OnQuickStep(InputAction.CallbackContext context);
+        void OnSpecialAttack(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/Scripts/Weapons/BroadswordAttack.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class BroadswordAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float smallAttackCooldown = 2.3f;
+    [SerializeField]
+    private float smallSwingLungeDelay = 1.2f;
+    [SerializeField]
+    private float smallSwingLungeTime = 0.5f;
+    [SerializeField]
+    private float smallSwingLungeForce = 30f;
+    [SerializeField]
+    private float smallSwingAttackDamage = 5.0f;
+
+    [SerializeField]
+    private float defendCooldown = 10f;
+
+    public LayerMask attackLayer;
+    public Animator animator;
+    Camera cam;
+
+    public GameObject hitEffect;
+    //public FMODUnity.EventReference fistSwingEvent;
+
+    private bool isAttacking = false;
+    private PlayerInput playerAttack;
+
+    private Movement playerMovement;
+
+    [SerializeField]
+    private Rigidbody playerRb;
+
+    // check these below
+
+    [SerializeField]
+    private float AttackRange = 2f;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        cam = Camera.main;
+        playerAttack = new PlayerInput();
+        playerAttack.Enable();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = player.GetComponent<Movement>();
+        playerRb = player.GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("Enabled Broadsword");
+        playerAttack.Player_Map.Attack.performed += Attack;
+        playerAttack.Player_Map.SpecialAttack.performed += SpecialAttack;
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Disabled Broadsword");
+        playerAttack.Player_Map.Attack.performed -= Attack;
+        playerAttack.Player_Map.SpecialAttack.performed -= SpecialAttack;
+    }
+
+    public void AttackHitBox()
+    {
+        // StartCoroutine(AttackRaycast(M1AttackRange, M1AttackDamage, M1AttackDelay));
+    }
+
+    private void Update()
+    {
+        // if (CountAttack == 1)
+        // {
+        //     animator.SetInteger("attackPhase", 1);
+        //     //StartCoroutine(AttackRaycast(M1AttackRange, M1AttackDamage, M1AttackDelay));
+        //     isAttacking = true;
+        // }
+    }
+
+    void HitTarget(Vector3 pos, GameObject hittable)
+    {
+        // play fist hit event
+        // FMODUnity.RuntimeManager.PlayOneShot(fistHitEvent);
+
+        GameObject GO = Instantiate(hitEffect, pos, Quaternion.identity);
+        GO.transform.parent = hittable.transform;
+        Destroy(GO, 20);
+    }
+
+    private IEnumerator AttackRaycast(float attackRange, float attackDamage, float attackDelay)
+    {
+
+        yield return new WaitForSeconds(attackDelay);
+        if (Physics.Raycast(cam.transform.position,
+        cam.transform.forward, out RaycastHit hit, attackRange, attackLayer))
+        {
+            if (hit.transform.TryGetComponent<Entity>(out Entity T))
+            {
+                HitTarget(hit.point, T.gameObject);
+                T.TakeDamage(attackDamage);
+            }
+        }
+    }
+
+    private void Attack(CallbackContext context)
+    {
+        if (isAttacking) return;
+
+        isAttacking = true;
+        animator.SetInteger("attack", 1);
+
+        // play fist swing event
+        //FMODUnity.RuntimeManager.PlayOneShot(fistSwingEvent);
+
+        StartCoroutine(ResetAttackLockIn(smallAttackCooldown));
+        StartCoroutine(ApplyForwardLunge(smallSwingLungeDelay, smallSwingLungeTime, smallSwingLungeForce));
+    }
+    
+    private IEnumerator ApplyForwardLunge(float attackDelay, float lungeTime, float lungeForce)
+    {
+        //apply force forwards
+        yield return new WaitForSeconds(attackDelay);
+        playerRb.velocity = new Vector3(transform.forward.x, 0, transform.forward.z) * lungeForce;
+        yield return new WaitForSeconds(lungeTime);
+        playerRb.velocity = Vector3.zero;
+    }
+
+    private IEnumerator ResetAttackLockIn(float attackCooldown)
+    {
+        yield return new WaitForSeconds(attackCooldown);
+        ResetAttackPhase();
+    }
+
+    private void SpecialAttack(CallbackContext context)
+    {
+        if (isAttacking) return;
+
+        isAttacking = true;
+
+        Debug.Log("Defend not implemented");
+
+        // animator.SetInteger("attackPhase", 2);
+
+        //FMODUnity.RuntimeManager.PlayOneShot(defendEvent);
+
+        StartCoroutine(ResetAttackLockIn(defendCooldown));
+
+    }
+
+    private void ResetAttackPhase()
+    {
+        animator.SetInteger("attack", 0);
+        isAttacking = false;
+    }
+}
